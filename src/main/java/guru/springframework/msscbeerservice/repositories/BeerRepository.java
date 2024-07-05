9b9b23ff288b7f3dbf0932a00d7f886c9572045b
OKTAY KARA <oktaykara14@gmail.com>
2024-07-05 14:51:23 +0300

    adding CircleCI config

---
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbeerservice.repositories;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+}
